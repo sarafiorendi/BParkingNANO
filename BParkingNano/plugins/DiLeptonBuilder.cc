a10ef647c979794ed66cc700d24ff48c49f2c13b
sara <sara.fiorendi@cern.ch>
2019-09-12 11:11:37 +0200

    fix lepton index

--- BParkingNano/plugins/DiLeptonBuilder.cc
+++ BParkingNano/plugins/DiLeptonBuilder.cc
@@ -85,10 +85,10 @@ void DiLeptonBuilder<Lepton>::produce(edm::StreamID, edm::Event &evt, edm::Event
       lepton_pair.setP4(l1_ptr->p4() + l2_ptr->p4());
       lepton_pair.setCharge(l1_ptr->charge() + l2_ptr->charge());
       lepton_pair.addUserFloat("lep_deltaR", reco::deltaR(*l1_ptr, *l2_ptr));
-      lepton_pair.addUserInt("l1_idx", l1_idx);
-      lepton_pair.addUserInt("l2_idx", l2_idx);
-      // Use UserCands as they should not use memory but keep the Ptr itself
       // Put the lepton passing the corresponding selection
+      lepton_pair.addUserInt("l1_idx", l1_as_l1 ? l1_idx : l2_idx);
+      lepton_pair.addUserInt("l2_idx", l1_as_l1 ? l2_idx : l1_idx);
+      // Use UserCands as they should not use memory but keep the Ptr itself
       lepton_pair.addUserCand("l1", l1_as_l1 ? l1_ptr : l2_ptr);
       lepton_pair.addUserCand("l2", l1_as_l1 ? l2_ptr : l1_ptr);
 
